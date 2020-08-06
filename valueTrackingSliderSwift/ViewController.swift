1a132fb634ccb2a021822f9f905efa121a84ecc5
e170pilot <e170pilot@users.noreply.github.com>
2020-08-06 13:54:16 +0300

    Initial Commit

---
+++ valueTrackingSliderSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  valueTrackingSliderSwift
+//
+//  Created by Mohammed on 06/08/2020.
+//  Copyright © 2020 Mohammed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
